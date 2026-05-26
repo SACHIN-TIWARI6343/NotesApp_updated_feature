d0c577cca9207a8752b6b5ea85fc360b8cdadac9
Sachin Tiwari <tiwarisachin6343@gmail.com>
2026-05-26 20:45:25 +0530

    logger completed

--- src/queues/emailQueue.js
+++ src/queues/emailQueue.js
@@ -4,7 +4,7 @@ const { Queue } = require("bullmq");
 // create a queue for email sending
 const redisConnectionUrl = process.env.REDIS_URL || `redis://${process.env.REDIS_HOST || "redis"}:${process.env.REDIS_PORT || 6379}`;
 
-const emailQueue = new Queue("emailQueue", {
+const emailQueue =  new Queue("emailQueue", {
   connection: {
     url: redisConnectionUrl,
   },
